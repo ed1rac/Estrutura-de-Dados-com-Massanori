3eea1298f3de493795163b3c1243ad2692ed072a
Sham Vinicius Fiorin <skatesham@yahoo.com.br>
2016-08-16 15:55:30 -0300

    Update Busca Binária Em C.c

--- Busca Binária Em C.c
+++ Busca Binária Em C.c
@@ -1,36 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int buscaBinaria(int x, int lista[],int n){
-    /*
-    Função de Busca Binária
-    Pesquisa um numero em uma lista ordenada
-    Tempo de O(Log(n)) e espaço de O(n)
-    Input:: int x, int lista[], int n
-    Output:: local do item buscado
-    */
-    int inicio = 0;
-    int fim = n-1;
-    int meio = fim/2;
-    while(fim-inicio>1){
-        if(x <= lista[meio] ){
-            fim -= meio;
-        }
-        else{
-            inicio += fim - meio;
-        }
-        meio =(fim - inicio)/2;
-    }
-    if (x <= lista[inicio]){
-        return inicio;
-    }
-    else{
-        return fim;
-    }
-}
-
-//                   TESTES 
-
+int buscaBinaria(int x, int lista[], int n);
 int main(){
     int x;
     int vetor[7] = {1,2,3,4,5,6,7};
@@ -55,4 +26,34 @@ int main(){
 
     system("pause");
     return 0;
+}
+
+int buscaBinaria(int x, int lista[],int n){
+    /*
+    Função de Busca Binária
+    Pesquisa um numero em uma lista ordenada
+    Tempo de O(Log(n)) e espaço de O(n)
+    Input:: int x, int lista[], int n
+    Output:: local do item buscado
+    */
+    int inicio = 0;
+    int fim = n-1;
+    int meio = fim/2;
+    while(fim-inicio>1){
+        if(x <= lista[meio] ){
+            fim -= meio;
+            meio = fim/2;
+        }
+        else{
+            inicio += fim - meio;
+            meio = inicio+((fim-inicio)/2);
+        }
+        if(x==lista[meio])  return meio;   // Contigência de Acerto ou mutiplos
+    }
+    if (x <= lista[inicio]){
+        return inicio;
+    }
+    else{
+        return fim;
+    }
 }
