f6e6446acdef1cd789c4d40675faded0d0d7369e
Sham Vinicius Fiorin <skatesham@yahoo.com.br>
2016-11-24 18:01:03 -0200

    Algoritmo Guloso - Grafos

--- guloso.py
+++ guloso.py
@@ -7,12 +7,33 @@ dicionario[5] = [7]
 dicionario[6] = [1,3]
 dicionario[7] = [1,2,3,4,5]
 
+dic = dict()
+dic[1] = [3]
+dic[2] = [3,5]
+dic[3] = [1, 2]
+dic[4] = [5]
+dic[5] = [2, 4] 
+
+diciona = dict()
+diciona[1] = [2,6,7,8,11]
+diciona[2] = [1,3,12]
+diciona[3] = [2,6]
+diciona[4] = [6,7]
+diciona[5] = [7,8]
+diciona[6] = [1,3,4]
+diciona[7] = [1,4,7]
+diciona[8] = [1,5,9]
+diciona[9] = [8,10,11]
+diciona[10] = [9]
+diciona[11] = [1,9]
+diciona[12] = [2]
+
 def removeGuloso(x, item):
     retirar = item[x]
-    for atual in dicionario:
+    for atual in item:
         novo = list()
         for j in item[atual]:
-            if j not in item[x] and j != x:
+            if j not in item[x] and j != x and j not in retirar:
                 novo.append(j)
             item[atual] = novo
     for i in retirar:
@@ -22,16 +43,16 @@ def removeGuloso(x, item):
 
 def guloso(grafo):
     comp = list()
-    while len(grafo) != 1:
+    while len(grafo) > 1:
         x = localizaGuloso(grafo)
         grafo = removeGuloso(x, grafo)
         comp.append(x)
-    for i in grafo:
-        comp.append(i)
+    if len(grafo) == 1:
+        for i in grafo:
+            comp.append(i)
     return comp
 
 def localizaGuloso(item):
-    comp = list()
     menor = len(item)
     for i in item:
         if len(item[i]) < menor:
@@ -39,6 +60,11 @@ def localizaGuloso(item):
             menor = len(item[i])
     return escolhido
 
-print(guloso(dicionario))
+print(dicionario)
+print("S = ",guloso(dicionario))
 print()
-print(guloso(dic))
+print(dic)
+print("S = ",guloso(dic))
+print()
+print(diciona)
+print("S = ",guloso(diciona))
